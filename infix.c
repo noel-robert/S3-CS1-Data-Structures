fa9ca0ccba7fc177aa3c3e9aa9e5c77c222a8410
NOEL ROBERT <93996838+noel060202@users.noreply.github.com>
2022-01-07 14:05:29 +0000

    corrected infix.c, still need to work on power(^)

--- infix.c
+++ infix.c
@@ -34,7 +34,7 @@ int priority(char ele)		// finding priority of operator
 void infix_to_postfix(char* ifx, char* pfx)
 {
 	char ch, temp;
-	int i = 0, p = 0;// counter variables, i to traverse through infix expression, p for postfix
+	int i=0, p=0;  // counter variables to traverse through infix and postfix expression
 	push('(');
 
 	while ((ch = ifx[i++]) != '\n')
@@ -63,6 +63,36 @@ void infix_to_postfix(char* ifx, char* pfx)
 	pfx[p] = 0;	
 }
 
+// <-- working postfix eval
+// int postfix_eval(char* pfx)
+// {
+// 	char ch;						
+// 	int a,b;						// operands
+// 	int i=0;
+	
+// 	while((ch = pfx[i++]) != 0)
+// 	{
+// 		if(isdigit(ch))
+// 			push(ch-'0');			// converts character to integer
+// 		else								// ch is an operator
+// 		{
+// 			b = pop();
+// 			a = pop();
+// 			double res;
+// 			switch(ch)
+// 			{
+// 				case '+' : push(a + b); break;
+// 				case '-' : push(a - b); break;
+// 				case '*' : push(a * b); break;
+// 				case '/' : push(a / b); break;
+// 				case '^' : res = pow((double)(a), (double)(b));
+// 										push(res); break;				 
+// 			}
+// 		}	
+// 	}
+// 	return stk[top];							// returns result
+// }
+
 int postfix_eval(char* pfx)
 {
 	char ch;						
@@ -71,8 +101,13 @@ int postfix_eval(char* pfx)
 	
 	while((ch = pfx[i++]) != 0)
 	{
-		if(isdigit(ch))
-			push(ch-'0');			// converts character to integer
+		if(isalpha(ch))
+		{
+			int value;
+			printf("Enter value of variable %c ", ch);
+			scanf("%d", &value);
+			push(value);
+		}
 		else								// ch is an operator
 		{
 			b = pop();
@@ -89,7 +124,6 @@ int postfix_eval(char* pfx)
 			}
 		}	
 	}
-
 	return stk[top];							// returns result
 }
 
@@ -97,16 +131,15 @@ int main()
 {
 	char ifx[1000], pfx[1000];
 
-	printf("Input infix expression without any spaces ");
-	
+	printf("Input infix expression without any spaces in alphabetic form ");
 	fgets(ifx, 1000, stdin);			// enter infix expression
 	
-
-	infix_to_postfix(ifx, pfx);	// convert infix to postfix expression
+	infix_to_postfix(ifx, pfx);		// convert infix to postfix expression
 
 	printf("Given infix expression is %s", ifx);
 	printf("Postfix expression is %s\n", pfx);
 
 	printf("Result after postfix evaluation is %d\n", postfix_eval(pfx));
+	
 	return 0;
 }
