2a6e09f622ba2c7bc5fd842f22d7741c34faa339
NOEL ROBERT <93996838+noel060202@users.noreply.github.com>
2022-02-26 17:27:17 +0000

    trees node deletion left

--- trees.c
+++ trees.c
@@ -1,7 +1,7 @@
 // Noel John Robert
 // B20CS1147
-// program to implement binary search tree operations
-
+// program to implement Binary Search Tree 
+							// deletion not done
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,49 +19,127 @@ void inorder_traversal(struct node *ptr)
 	if(ptr != NULL)
 	{
 		inorder_traversal(ptr->LC);
-		printf("%d", ptr->info);
+		printf("%d ", ptr->info);
 		inorder_traversal(ptr->RC);
 	}	
 	else
 		return;
 }
 
-void insert(int item)
+void preorder_traversal(struct node *ptr)
 {
-	
+	if(ptr != NULL)
+	{
+		printf("%d ", ptr->info);
+		preorder_traversal(ptr->LC);
+		preorder_traversal(ptr->RC);
+	}	
+	else
+		return;
 }
 
-struct node* searchParent(int item)
+void postorder_traversal(struct node *ptr)
+{
+	if(ptr != NULL)
+	{
+		postorder_traversal(ptr->LC);
+		postorder_traversal(ptr->RC);
+		printf("%d ", ptr->info);
+	}	
+	else
+		return;
+}
+
+bool search(int item)
 {
 	struct node* ptr = root;
-	struct node* parent = NULL;
 	
 	while(ptr!=NULL && ptr->info!=item)
 	{
-		parent = ptr;
 		if(ptr->info < item)
 			ptr = ptr->RC;
 		else
 			ptr = ptr->LC;
-	}	
-	return parent;
+	}
+	
+	if(ptr == NULL)
+		return false;
+	else if(ptr->info == item)
+		return true;		
 }
 
-boolean search(int item)
+void insert(int item)
 {
-	struct node* parent = searchParent(item);
+	// create and initialize new node	
+	struct node* temp = (struct node*) malloc (sizeof(struct node)); 
+	temp->LC = NULL;
+	temp->info = item;
+	temp->RC = NULL;
+	
+	if(root == NULL)	// empty tree
+	{
+		root = temp;
+		return;
+	}
 
-	if(parent == NULL)
-		return false;
-	else if((parent->LC)->info == item || (parent->RC)->info == item)
-		return true;
+	if(search(item))   // item already present
+	{
+		printf("Item already present\n");
+		return;
+	}  
+	
+	struct node* ptr = root;
+	struct node* parent = NULL;
+	while(ptr!=NULL)
+	{
+		parent = ptr;
+		if(ptr->info > item)
+			ptr = ptr->LC;
+		else
+			ptr = ptr->RC;
+	}
+	
+	if(parent->info > item)
+		parent->LC = temp;
 	else
-		return false;
+		parent->RC = temp;
 }
 
-void height()
+//struct node* searchParent(int item)
+//{
+//	struct node* ptr = root;
+//	struct node* parent = NULL;
+//	
+//	while(ptr!=NULL && ptr->info!=item)
+//	{
+//		parent = ptr;
+//		if(ptr->info < item)
+//			ptr = ptr->RC;
+//		else
+//			ptr = ptr->LC;
+//	}	
+//	return parent;
+//}
+
+void deletion(int item)
 {
 	
+}
+
+int height(struct node *ptr)
+{
+	if(ptr == NULL)
+		return -1;
+	else
+	{
+		int l = height(ptr->LC);
+		int r = height(ptr->RC);
+
+		if(l > r)
+			return l;		// or l+1
+		else
+			return r;		// or r+1
+	}	
 }
 
 int main()
@@ -70,28 +148,46 @@ int main()
 	do
 	{
 		printf("\n1. Insert node\n");
-		printf("2. Search\n");
-		printf("3. Height\n");
-		printf("4. Exit program\n");
-		scanf("%d", &code);							// input choice here
+		printf("2. Search for element\n");
+		printf("3. Delete node\n");
+		printf("4. Height of tree\n");
+		printf("5. Inorder traversal\n");
+		printf("6. Exit program\n");
+		scanf("%d", &code);		// input choice here
 
 		int num;
 		switch(code)
 		{
-			case 1:	printf("Input number to insert: "); scanf("%d", &num);
-							insert(num); break;
+			case 1: printf("Input number to insert: "); scanf("%d", &num);
+				 			insert(num); break;
 			
-			case 2:	printf("Input number to search: "); scanf("%d", &num);
-							search(num); break;
+			case 2: printf("Input number to search: "); scanf("%d", &num);
+				 			if(search(num))
+								printf("Item is present\n");
+							else
+								printf("Item not present\n");
+							break;
+				 
+			case 3: printf("Input number to delete: "); scanf("%d", &num);
+				 			deletion(num); break;
+				 
+			case 4: height(root); break;
+
+			case 5: inorder_traversal(root);
+							preorder_traversal(root);
+							postorder_traversal(root);
+							printf("\n"); break;
 			
-			case 3: height(); break;
-			
-			case 4: printf("Exiting program on user input\n"); break;
+			case 6: printf("Exiting program on user input\n"); break;
 			
 			default:printf("Wrong code inputted\n");
 		}
-	}while(code != 4);
-	inorder_traversal(root);
-	
+	}while(code != 6);
+
 	return 0;
 }
+
+/*
+OUTPUT
+
+*/
